171868ef7279dbc8bac422788ca9117e801b93a1
ridhoriky <ridhoestu.b@gmail.com>
2023-11-20 20:40:20 +0700

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-[#15191e] text-base-content ">
+    <footer className="footer footer-center p-4 text-white/60 bg-[#15191e]">
       <aside>
         <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
       </aside>
