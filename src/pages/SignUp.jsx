6523ac27fff33a5246ffe8f6eb49deac340d163b
ridhoriky <ridhoestu.b@gmail.com>
2023-12-05 19:34:06 +0700

    fix background and responsive

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -56,8 +56,8 @@ const SignUp = () => {
       />
       <div className=" fixed top-0 left-0 w-full h-screen"></div>
       <div className="fixed w-full px-4 py-40 z-50">
-        <div className="max-w-[450px] h-[600px] mx-auto rounded-md bg-white/95 sm:bg-white/80 text-gray-900">
-          <div className="max-w-[320px] mx-auto py-10 md:y-20">
+        <div className="max-w-[450px] h-[500px] md:h-[600px] mx-auto rounded-md bg-white/70 sm:bg-white/80 text-gray-900">
+          <div className="max-w-[320px] mx-auto py-10 md:py-20">
             <h1 className="text-3xl font-bold">Sign Up</h1>
             <form
               onSubmit={handleSubmit}
