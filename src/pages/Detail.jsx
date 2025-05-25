71cbf344aeb6a2e17a59b2f6e059e2ea2006e009
ridhoestu.b_mti <ridhoestu.b@gmail.com>
2025-05-25 20:53:16 +0700

    fix: add atribut performa

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import GlobalAPI from '../sevices/GlobalAPI';
-import { db } from '../sevices/Firebase';
-import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
-import { useParams } from 'react-router';
-import { UserAuth } from '../context/AuthContext';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { useLike } from '../context/LikeContext';
+import React, { useState, useEffect } from "react";
+import GlobalAPI from "../sevices/GlobalAPI";
+import { db } from "../sevices/Firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { useParams } from "react-router";
+import { UserAuth } from "../context/AuthContext";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useLike } from "../context/LikeContext";
 const IMAGE_BASE_URL = import.meta.env.VITE_BASE_IMG_URL;
 
 const DetailPage = () => {
@@ -23,7 +23,7 @@ const DetailPage = () => {
     });
   }, [id, type]);
 
-  const movieID = user?.email ? doc(db, 'users', user.email) : null;
+  const movieID = user?.email ? doc(db, "users", user.email) : null;
 
   const saveShow = async () => {
     if (user?.email && movieID) {
@@ -37,7 +37,7 @@ const DetailPage = () => {
         }),
       });
     } else {
-      alert('Please log in to save a movie');
+      alert("Please log in to save a movie");
     }
   };
 
@@ -76,29 +76,34 @@ const DetailPage = () => {
               className="w-[280px] md:w-full h-[400px] md:h-[620px] object-cover rounded-md my-3 xl:my-5"
               src={
                 details?.poster_path === null || details?.profile_path === null
-                  ? 'https://img.freepik.com/free-vector/coming-soon-banner-with-focus-lights_1017-33739.jpg?size=626&ext=jpg&ga=GA1.1.492795408.1700300440&semt=ais'
+                  ? "https://img.freepik.com/free-vector/coming-soon-banner-with-focus-lights_1017-33739.jpg?size=626&ext=jpg&ga=GA1.1.492795408.1700300440&semt=ais"
                   : url
               }
               alt={details?.title || details?.name}
+              loading={details?.poster_path ? "eager" : "lazy"}
+              fetchpriority="high"
+              width={280}
+              height={400}
+              decoding="async"
             />
           </div>
           <div className="w-full xl:w-2/3 pl-2 xl:pl-10  my-4 xl:my-5">
             <h2 className="text-3xl mb-5">{details?.title || details?.name}</h2>
             {details?.birthday && (
               <div className="font-semibold text-white/80 mb-4">
-                Birthday:{' '}
+                Birthday:{" "}
                 <span className="font-light">{details?.birthday}</span>
               </div>
             )}
 
             {details?.genres && (
               <div className="font-semibold text-white/80 mb-4">
-                Genres:{' '}
+                Genres:{" "}
                 <span>
                   {details?.genres?.map((genre, index) => (
                     <span className="font-light" key={genre.id}>
                       {genre.name}
-                      {index !== details.genres.length - 1 && ', '}
+                      {index !== details.genres.length - 1 && ", "}
                     </span>
                   ))}
                 </span>
@@ -107,21 +112,21 @@ const DetailPage = () => {
 
             {details?.popularity && (
               <div className="font-semibold text-white/80 mb-4">
-                Popularity:{' '}
+                Popularity:{" "}
                 <span className="font-light">{details?.popularity}</span>
               </div>
             )}
 
             {details?.release_date && (
               <div className="font-semibold text-white/80 mb-4">
-                Released:{' '}
+                Released:{" "}
                 <span className="font-light">{details?.release_date}</span>
               </div>
             )}
 
             {details?.first_air_date && (
               <div className="font-semibold text-white/80 mb-4">
-                Released:{' '}
+                Released:{" "}
                 <span className="font-light">{details?.first_air_date}</span>
               </div>
             )}
@@ -132,14 +137,16 @@ const DetailPage = () => {
               {isLiked(details.id) ? (
                 <span
                   onClick={deleteShow}
-                  className="flex items-center justify-center w-fit rounded-md gap-2 border-2 p-2 hover:text-black hover:bg-white duration-300 ease-in-out">
+                  className="flex items-center justify-center w-fit rounded-md gap-2 border-2 p-2 hover:text-black hover:bg-white duration-300 ease-in-out"
+                >
                   <FaHeart size={24} title="Delete From Wishlist" /> Delete From
                   Wishlist
                 </span>
               ) : (
                 <span
                   onClick={saveShow}
-                  className="flex items-center justify-center w-fit rounded-md gap-2 border-2 p-2 hover:text-black hover:bg-white duration-300 ease-in-out">
+                  className="flex items-center justify-center w-fit rounded-md gap-2 border-2 p-2 hover:text-black hover:bg-white duration-300 ease-in-out"
+                >
                   <FaRegHeart size={24} title="Add to Wishlist" /> Add to
                   Wishlist
                 </span>
